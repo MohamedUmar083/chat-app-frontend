b56482cb7218489fa7df83badc133e70bedbe005
MohamedUmar083 <umarmohd777@gmail.com>
2024-09-24 21:20:37 +0530

    Changes according to New Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import LoginUser from "./Pages/LoginUser";
 import RegisterUser from "./Pages/RegisterUser";
 import Chat from "./Pages/Chat";
+import Home from "./Pages/Home";
 
 const App = () => {
   return (
-    <div>
+    <div className="homepage">
       <Routes>
-        <Route path="/" element={<LoginUser />} />
-        <Route path="/register" element={<RegisterUser />} />
+        {/* <Route path="/" element={<LoginUser />} />
+        <Route path="/register" element={<RegisterUser />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="/chats" element={<Chat />} />
       </Routes>
     </div>
